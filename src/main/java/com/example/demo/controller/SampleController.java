f0c8be87cb54ef3499f7158bfcc63233d1e44b9f
Hong Youl <hongyoul03@gmail.com>
2024-09-24 11:06:11 +0900

    5장 끝

--- src/main/java/com/example/demo/controller/SampleController.java
+++ src/main/java/com/example/demo/controller/SampleController.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.demo.dto.SampleDTO;
+
 
 
 
@@ -46,15 +48,60 @@ public class SampleController {
 		model.addAttribute("dto", dto);	 // 화면에 객체 전달
 	}
 	
-	@GetMapping("/ex5")
-	public void ex5(Model model) {
-		
+//	@GetMapping("/ex5")
+//	public void ex5(Model model) {
+//		
+//		List<SampleDTO> list = new ArrayList<>();
+//		
+//		list.add(new SampleDTO(1, "aaa", LocalDate.now()));
+//		list.add(new SampleDTO(2, "bbb", LocalDate.now()));
+//		list.add(new SampleDTO(3, "ccc", LocalDate.now()));
+//		
+//		model.addAttribute("list", list); // 화면에 리스트 전달
+//	}
+	
+	
+	// @GetMapping에 {}를 사용하여 여러 URL을 처리할 수 있음
+	// 예: /ex5", "/ex6 ... 요청이 들어오면 동일한 메소드가 호출됨
+	// URL은 다르지만 화면에 동일한 데이터를 전달
+	
+	@GetMapping({"/ex5", "/ex6", "/ex7", "/ex8"})
+	public void ex6(Model model) {
 		List<SampleDTO> list = new ArrayList<>();
 		
 		list.add(new SampleDTO(1, "aaa", LocalDate.now()));
-		list.add(new SampleDTO(2, "bbb", LocalDate.now()));
-		list.add(new SampleDTO(3, "ccc", LocalDate.now()));
+		list.add(new SampleDTO(2, "ccc", LocalDate.now()));
+		list.add(new SampleDTO(3, "ddd", LocalDate.now()));
+//		list.add(new SampleDTO(4, "bbb", LocalDate.now()));
 		
 		model.addAttribute("list", list); // 화면에 리스트 전달
+	}
+
+	@GetMapping("/ex9")
+	public void ex9(Model model) {
+		
+		List<SampleDTO> list =new ArrayList();
+		
+		for (int i = 1; i <= 20; i++) {
+			list.add(new SampleDTO(i, i+"번", LocalDate.now()));
+			
+		}
+		
+		model.addAttribute("list", list);
+		
+	}
+	
+	@GetMapping("/ex10")
+	public void ex10(Model model) {
+		SampleDTO dto = new SampleDTO(1, "aaa", LocalDate.now());
+		
+		model.addAttribute("result", "success"); // 문자열 전달
+		model.addAttribute("dto", dto); // 객체 전달
+	}
+	
+	
+	@GetMapping("/ex11")
+	public void ex11(Model model) {
+		model.addAttribute("date", LocalDateTime.now());
 	}
 }
--- src/main/java/com/example/demo/controller/SampleQuiz.java
+++ src/main/java/com/example/demo/controller/SampleQuiz.java
@@ -1,5 +1,7 @@
 package com.example.demo.controller;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/quiz")
+
 public class SampleQuiz {
 
+	
+	// model: 뷰에 데이터를  전달하는 객체
+	// void: URL 경로에 따라 HTML 파일 반환
 	
 	@GetMapping("/q1") 
 	public void q1(Model model) {
@@ -21,7 +27,12 @@ public class SampleQuiz {
 	public void q2(Model model) {
 		model.addAttribute("msg4","스프링부트웹프로젝트");
 		model.addAttribute("msg5","구멍가게 코딩단");
-		model.addAttribute("msg6","2022-12-25");
+		
+		model.addAttribute("msg6","2022.12.25");
+		model.addAttribute("msg7", LocalDate.of(2022,12,25));
 	}
+	
+	
+	
 	
 }
